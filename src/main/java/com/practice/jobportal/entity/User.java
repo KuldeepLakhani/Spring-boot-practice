13263d3e9b268506f9ad1593a5d3fdca6ccf71cc
Kuldeep Lakhani <kuldeeplakhani404@gmail.com>
2026-01-29 17:08:16 +0530

    Add User and Role entities

---
+++ src/main/java/com/practice/jobportal/entity/User.java
@@ -0,0 +1,27 @@
+package com.kuldeep.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
